0df23611456957cf9f4c9366fadd30ca73a16741
Litvinov Yuriy <litvinov.yuriy1996@gmail.com>
2021-05-20 13:58:25 +0400

    mobile

--- js/script.js
+++ js/script.js
@@ -26,13 +26,44 @@ $(document).ready(function() {
 
     $('.variants__filter-item--year .variants__filter-item_value').text((new Date).getFullYear());
 
-    $('.banner__form-btn button').click(() => {
+    $('.banner__form-btn button, .banner__form-btn--mobile').click(() => {
         const mounth = $('.banner-form-select--mounth .current').text().trim();
         const country = $('.banner-form-select--countries .current').text().trim();
         $('.variants__filter-item--mounth .variants__filter-item_value').text(mounth);
         $('.variants__filter-item--country_flag').html(window.countries.find(el => el.name === country).flag);
         $('.variants__filter-item--country_name').html(country);
         $('.variants').addClass('show');
+    });
+
+    $('button[data-callme="true"]').click(() => {
+        $('.callme').addClass('show');
+        $('body, html').css('overflow', 'hidden');
+    });
+
+    $('.callme__type').click(e => {
+        e.preventDefault();
+        if (!$(e.currentTarget).hasClass('active')) {
+            $('.callme__type').each((i, el) => {
+                $(el).removeClass('active');
+            });
+            $(e.currentTarget).addClass('active');
+        }
+    });
+
+    $('.callme__amount').click(e => {
+        e.preventDefault();
+        if (!$(e.currentTarget).hasClass('active')) {
+            $('.callme__amount').each((i, el) => {
+                $(el).removeClass('active');
+            });
+            $(e.currentTarget).addClass('active');
+        }
+    });
+
+    $('.callme__container').click(e => e.stopPropagation());
+    $('.callme, .callme__close').click(e => {
+        $('body, html').css('overflow', 'auto');
+        $('.callme').removeClass('show');
     });
 });
 
@@ -43,8 +74,8 @@ function addFlag() {
 }
 
 async function getCountries() {
-    const url = document.URL;
-    const res = await fetch(`${url}js/countries.json`);
+    const url = window.origin;
+    const res = await fetch(`${url}/js/countries.json`);
     const countries = res.ok ? (await res.json()) : [];
     return countries;
 }
